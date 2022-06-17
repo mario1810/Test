8d9d504239165b3a757a84e2e9d0e8af07814193
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-06-17 15:10:36 -0500

    se realiza el cambio dinámico de h2

--- saludo/script.js
+++ saludo/script.js
@@ -1,2 +1,26 @@
+//Linea de prueba para saber que está bien la conexión
 console.log("Me parece que todavia me extrañas");
+
+let temp=setInterval(cambiarSaludo,2000);
+let frase=document.getElementsByTagName("h3")[0];
+//frase.innerText="hola";
+//console.log(frase);    
+
+let  key =true;
+function cambiarSaludo(){
+
+    
+    if(key==true){
+        //document.getElementsByTagName("h3")[0].innerHTML="impongo moda";"
+        frase.innerText="Impongo moda";
+        key=false;
+    }else{
+        frase.innerText="Ganando como siempre";
+       key=true; 
+    }
+
+    //console.log("frase :"+frase);
+    //console.log("Me parece una falta de respeto");
+    //clearInterval(temp);
+}
 
